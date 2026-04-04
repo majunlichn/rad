12b45178cd817eca7fad4d1a84506141f8feebd6
Jerry Ma <majunlichn@outlook.com>
2026-04-04 11:36:07 +0800

    rad/Common/Memory

---
+++ rad/Common/Memory.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <rad/Common/Platform.h>
+
+#include <memory>
+
+namespace rad
+{
+
+// Allocate `size` bytes aligned to `alignment` bytes.
+// - `alignment` must be a power of two.
+// - `size` will be rounded up to a multiple of `alignment`.
+void *AlignedAlloc(std::size_t size, std::size_t alignment);
+// Free memory returned by `AlignedAlloc`.
+void AlignedFree(void *p);
+
+} // namespace rad
