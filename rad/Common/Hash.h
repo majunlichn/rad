dfa7d9a55fe7be2702f8259e788c6b38a810ce6c
Jerry Ma <majunlichn@outlook.com>
2026-04-05 00:26:08 +0800

    rad/Common/Hash
    - pcg_hash32
    - pcg_hash64
    - MurmurHash3: https://github.com/aappleby/smhasher

--- rad/Common/Hash.h
+++ rad/Common/Hash.h
@@ -1,17 +1,27 @@
 #pragma once
 
 #include <rad/Common/Integer.h>
+#include <rad/Common/MurmurHash3.h>
 
 namespace rad
 {
 
+// For general-purpose, non-cryptographic hashing.
 // https://www.jcgt.org/published/0009/03/02/
 // https://www.pcg-random.org/
-uint32_t hash_pcg(uint32_t v)
+uint32_t pcg_hash32(uint32_t v)
 {
     uint32_t state = v * 747796405u + 2891336453u;
     uint32_t word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
     return (word >> 22u) ^ word;
+}
+
+// For general-purpose, non-cryptographic hashing.
+uint64_t pcg_hash64(uint64_t v)
+{
+    uint64_t state = v * 6364136223846793005ull + 1442695040888963407ull;
+    uint64_t word = ((state >> ((state >> 59u) + 5u)) ^ state) * 12605985483714917081ull;
+    return (word >> 43u) ^ word;
 }
 
 } // namespace rad
