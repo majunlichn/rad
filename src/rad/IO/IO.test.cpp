f274231355c3e48bbbc57ef90b3b9b004f4b415d
Jerry Ma <majunlichn@outlook.com>
2026-04-26 23:31:37 +0800

    rad/IO: TestEnvironment

--- src/rad/IO/IO.test.cpp
+++ src/rad/IO/IO.test.cpp
@@ -1,7 +1,23 @@
-#include <rad/Container/Container.test.h>
+#include <rad/IO/IO.test.h>
+
+TestEnvironment::TestEnvironment(int argc, char** argv)
+{
+}
+
+void TestEnvironment::SetUp()
+{
+}
+
+void TestEnvironment::TearDown()
+{
+}
+
+TestEnvironment* g_env = nullptr;
 
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
+    g_env = new TestEnvironment(argc, argv);
+    testing::AddGlobalTestEnvironment(g_env);
     return RUN_ALL_TESTS();
 }
--- src/rad/IO/IO.test.h
+++ src/rad/IO/IO.test.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include <gtest/gtest.h>
+
+class TestEnvironment : public ::testing::Environment
+{
+public:
+    TestEnvironment(int argc, char** argv);
+
+    void SetUp() override;
+    void TearDown() override;
+
+private:
+
+}; // TestEnvironment
+
+extern TestEnvironment* g_env;
