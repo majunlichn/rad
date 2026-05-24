30cfc0ce93c51e0ad6ca5e6c151bf9e9437f790f
Jerry Ma <majunlichn@outlook.com>
2026-05-24 16:05:23 +0800

    rad/Common/Memory: AlignedAllocator

--- src/rad/Common/Memory.h
+++ src/rad/Common/Memory.h
@@ -2,6 +2,8 @@
 
 #include <rad/Common/Platform.h>
 
+#include <boost/align/aligned_allocator.hpp>
+
 #include <memory>
 
 namespace rad
@@ -13,5 +15,8 @@ namespace rad
 void* AlignedAlloc(std::size_t size, std::size_t alignment);
 // Free memory returned by `AlignedAlloc`.
 void AlignedFree(void* p);
+
+template <typename T, size_t Alignment>
+using AlignedAllocator = boost::alignment::aligned_allocator<T, Alignment>;
 
 } // namespace rad
