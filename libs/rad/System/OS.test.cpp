75f299ae189f458f9dd4f48846d1e9355e8b98cc
Jerry Ma <majunlichn@outlook.com>
2026-07-23 12:50:25 +0800

    rad/System/OS

---
+++ libs/rad/System/OS.test.cpp
@@ -0,0 +1,32 @@
+#include <rad/System/OS.h>
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+
+TEST(System, OS)
+{
+    EXPECT_GT(rad::os::getpid(), 0U);
+    EXPECT_FALSE(rad::os::get_exec_path().empty());
+
+    EXPECT_TRUE(rad::os::path::isabs(rad::os::path::realpath("missing-component")));
+    const auto [dotRoot, dotExtension] = rad::os::path::splitext("....jpg");
+    EXPECT_EQ(dotRoot, "....jpg");
+    EXPECT_TRUE(dotExtension.empty());
+
+#if defined(RAD_OS_WINDOWS)
+    EXPECT_EQ(rad::os::path::normcase(rad::os::path::commonpath({R"(C:\Foo\a)", R"(c:\foo\b)"})),
+              R"(c:\foo)");
+    EXPECT_THROW((void)rad::os::path::commonpath({R"(C:\foo)", R"(D:\foo)"}),
+                 std::invalid_argument);
+
+    const auto [drive, tail] = rad::os::path::splitdrive(R"(\\server\share\directory)");
+    EXPECT_EQ(drive, R"(\\server\share)");
+    EXPECT_EQ(tail, R"(\directory)");
+    EXPECT_TRUE(rad::os::path::ismount(rad::os::getcwd().root_path()));
+#endif
+}
